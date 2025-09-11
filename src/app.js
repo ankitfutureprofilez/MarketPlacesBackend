08e16032644c98cd649c215934e0d2a98936c8c5
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-09-11 12:45:27 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+// require("./mongoconfig");
+dotenv.config();
+
+const express = require("express");
+const multer = require("multer");
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+    origin: "*", // Allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*', // Allow all headers
+    credentials: true,
+    optionsSuccessStatus: 200, // for legacy browsers
+}
+
+// Apply CORS middleware globally
+app.use(cors(corsOptions));
+
+// Parsing middlewares
+app.use(express.json({ limit: '2000mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.REACT_APP_SERVER_DOMIN || 5000; // Add default port
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: 'Hello World',
+        status: 200,
+    });
+});
+
+const server = app.listen(PORT, () => console.log("Server is running at port : " + PORT));
+server.timeout = 360000; // 6 minutes
