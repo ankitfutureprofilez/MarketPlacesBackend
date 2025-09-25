f4fa4cfcb801f4740b97b3b96e128252c19bcdcf
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-09-25 17:01:55 +0530

    Main

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 require("./monogconfig");
 
 const express = require("express");
+const admin = require('firebase-admin');
 const app = express();
 const cors = require("cors");
 
@@ -14,14 +15,39 @@ const corsOptions = {
     optionsSuccessStatus: 200, // for legacy browsers
 }
 
-// Apply CORS middleware globally
 app.use(cors(corsOptions));
 
-// Parsing middlewares
 app.use(express.json({ limit: '2000mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.REACT_APP_SERVER_DOMIN || 5000; // Add default port
+const serviceAccount = require('./otpdemo-dc63b-firebase-adminsdk-fbsvc-bc859b783f.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+app.post('/send-notification', async (req, res) => {
+  try {
+    const { token, title, body } = req.body;
+
+    const message = {
+      token,
+      notification: { title, body },
+      webpush: {
+        fcmOptions: {
+          link: 'http://localhost:3000'
+        }
+      }
+    };
+
+    const response = await admin.messaging().send(message);
+    res.send({ success: true, response });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ success: false, error: err.message });
+  }
+});
+
+const PORT = process.env.REACT_APP_SERVER_DOMIN || 5000; 
 
 app.get("/", (req, res) => {
     res.json({
@@ -33,10 +59,8 @@ app.get("/", (req, res) => {
 app.use("/api/vendor", require("./route/VendorRoute"));
 app.use("/api/offer", require("./route/OfferRoute"));
 app.use("/api/user", require("./route/UserRoute"));
-app.use("/api/", require("./route/UserRoute"));
 
 
 
 const server = app.listen(PORT, () => console.log("Server is running at port : " + PORT));
 server.timeout = 360000;
-
