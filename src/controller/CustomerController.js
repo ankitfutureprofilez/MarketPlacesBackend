1bcabf0ecd02014c7d76f3a64f25257c4734c2b0
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-10-03 17:01:40 +0530

    Added custome rlogin and register

---
+++ src/controller/CustomerController.js
@@ -0,0 +1,50 @@
+const User = require("../model/User");
+
+exports.CustomerRegister = catchAsync(async (req, res) => {
+    try {
+        const {
+            name,
+            phone,
+            email
+        } = req.body;
+        
+        if (!name || !phone) {
+            return errorResponse(res, "Name and phone are required", 400);
+        }
+
+        const Users = await User.findOne({ phone: phone });
+        if (Users) {
+            return errorResponse(res, "Phone number already exists", 400,);
+        }
+        const userdata = new User({ name, phone, role: "customer", email });
+        const savedUser = await userdata.save();
+        if (!savedUser) {
+            return errorResponse(res, "Failed to create user", 500);
+        }
+        const token = jwt.sign(
+            { id: savedUser._id, role: savedUser.role, email: savedUser.email },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: process.env.JWT_EXPIRES_IN || "24h" }
+        );
+        return successResponse(res, "User created successfully", 201, {
+            user: savedUser,
+            token: token,
+            role: savedUser?.role,
+        });
+    } catch (error) {
+        return errorResponse(res, error.message || "Internal Server Error", 500);
+    }
+});
+
+exports.CustomerGet = catchAsync(async (req, res) => {
+    try{
+        const userId = req.user.id;
+        const customer = await User.findById(userId);
+        if (!customer) {
+            return errorResponse(res, "No customers found", 404);
+        }
+        return successResponse(res, "Customers retrieved successfully", 200, customers);        
+    }catch(error){
+        return errorResponse(res, error.message || "Internal Server Error", 500);
+    }
+});
---
+++ src/route/customerRoutes.js
@@ -0,0 +1,9 @@
+const { CustomerRegister } = require("../controller/CustomerController");
+const { verifyToken } = require("../utils/tokenVerify");
+
+const router = require("express").Router();
+
+router.post("/customer/add",  CustomerRegister);
+router.get("/customer/get_details", verifyToken, GetOfferId);
+
+module.exports = router;
