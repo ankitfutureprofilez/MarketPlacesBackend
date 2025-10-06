9a422f5155f2fec729ba8a7ad598320a9da4a275
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-10-06 13:26:17 +0530

    razpay  working  fine

--- src/app.js
+++ src/app.js
@@ -15,11 +15,67 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.use(express.json({ limit: "2000mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Hello World",
+    status: 200,
+  });
+});
+
+app.use("/api", require("./route/VendorRoute"));
+app.use("/api", require("./route/OfferRoute"));
+app.use("/api", require("./route/UserRoute"));
+app.use("/api", require("./route/salesRoute"));
+app.use("/api", require("./route/AdminRoute"));
+app.use("/api", require("./route/customerRoutes"));
+
+const server = app.listen(PORT, () =>
+  console.log("Server is running at port : " + PORT)
+);
+server.timeout = 360000;
+
+
+
+// const serviceAccount = require('../otpdemo-dc63b-firebase-adminsdk-fbsvc-bc859b783f.json');
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// app.post('/send-notification', async (req, res) => {
+//   try {
+//     const { token, title, body } = req.body;
+
+//     const message = {
+//       token,
+//       notification: { title, body },
+//       webpush: {
+//         fcmOptions: {
+//           link: 'http://localhost:3000'
+//         }
+//       }
+//     };
+
+//     const response = await admin.messaging().send(message);
+//     res.send({ success: true, response });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send({ success: false, error: err.message });
+//   }
+// });
+
+
+// // Razorpay instance
 // const Razorpay = require("razorpay");
 // const crypto = require("crypto");
 
 
-// // Razorpay instance
+
 // const razorpay = new Razorpay({
 //   key_id: "rzp_test_RQ3O3IWq0ayjsg",    // aapka Key ID
 //   key_secret: "RcwuasbTHAdmm1mrZTiigw2x",   // aapka Secret Key
@@ -88,61 +144,3 @@ app.use(cors(corsOptions));
 //     }
 //   }
 // );
-
-
-app.use(express.json({ limit: "2000mb" }));
-app.use(express.urlencoded({ extended: true }));
-
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.json({
-    msg: "Hello World",
-    status: 200,
-  });
-});
-
-app.use("/api", require("./route/VendorRoute"));
-app.use("/api", require("./route/OfferRoute"));
-app.use("/api", require("./route/UserRoute"));
-app.use("/api", require("./route/salesRoute"));
-app.use("/api", require("./route/AdminRoute"));
-app.use("/api", require("./route/customerRoutes"));
-
-
-
-
-const server = app.listen(PORT, () =>
-  console.log("Server is running at port : " + PORT)
-);
-server.timeout = 360000;
-
-
-
-// const serviceAccount = require('../otpdemo-dc63b-firebase-adminsdk-fbsvc-bc859b783f.json');
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
-// app.post('/send-notification', async (req, res) => {
-//   try {
-//     const { token, title, body } = req.body;
-
-//     const message = {
-//       token,
-//       notification: { title, body },
-//       webpush: {
-//         fcmOptions: {
-//           link: 'http://localhost:3000'
-//         }
-//       }
-//     };
-
-//     const response = await admin.messaging().send(message);
-//     res.send({ success: true, response });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send({ success: false, error: err.message });
-//   }
-// });
-
