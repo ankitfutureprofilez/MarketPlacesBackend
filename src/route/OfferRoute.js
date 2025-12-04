9db7dace2c6ba8b20b66ff584b3bbcee0db648b7
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-12-04 14:26:24 +0530

    verify token added

--- src/route/OfferRoute.js
+++ src/route/OfferRoute.js
@@ -6,7 +6,7 @@ const router = require("express").Router();
 
 router.post("/offer/add", verifyToken, upload.single("image"), AddOffer);
 router.get("/offer/get_details/:id", GetOfferId);
-router.post("/offer/offer_delete/:id", OfferDelete);
+router.post("/offer/offer_delete/:id", verifyToken, OfferDelete);
 router.get("/offer/get", verifyToken ,  GetOffer);
 router.get("/offer/status/:id/:status", verifyToken, OfferStatus);
 router.post("/offer/update/:id", verifyToken, upload.single("image"), EditOffer);
