c6b6eb02c23242663c95a130348eba8062de8b21
merndevfp01-ctrl <mern.dev.fp01@gmail.com>
2026-02-18 18:35:39 +0530

    Fix bugs

--- src/controller/EnquiryController.js
+++ src/controller/EnquiryController.js
@@ -1,4 +1,5 @@
-const Enquiry = require('../model/enquiry.js')
+const Enquiry = require('../model/enquiry.js');
+const { options } = require('../route/AdminRoute.js');
 const catchAsync = require("../utils/catchAsync");
 const { errorResponse, successResponse } = require("../utils/ErrorHandling");
 
@@ -11,7 +12,7 @@ exports.createEnquiry = catchAsync(async (req, res) => {
             return errorResponse(res, "This enquiry is already exist");
         }
 
-        if(!firstName || !lastName || !email || !phone || !message || !role){
+        if (!firstName || !lastName || !email || !phone || !message || !role) {
             return errorResponse(res, "Fields are required", 400);
         }
 
@@ -33,9 +34,19 @@ exports.createEnquiry = catchAsync(async (req, res) => {
 
 exports.getEnquiry = catchAsync(async (req, res) => {
     try {
-        const enquiries = await Enquiry.find();
-        if (!enquiries) {
-            return errorResponse(res, "Failed to fetch enquiries.")
+        const { search } = req.query;
+        const filter = {};
+
+        if (search) {
+            filter.$or = [
+                { firstName: { $regex: search.trim(), $options: "i" } },
+                { lastName: { $regex: search.trim(), $options: "i" } }
+            ]
+        }
+        const enquiries = await Enquiry.find(filter).lean();
+
+        if (!enquiries.length) {
+            return errorResponse(res, "No enquiries found.")
         }
         return successResponse(res, "All Enquiries fetched successfully", 200, enquiries)
     } catch (error) {
