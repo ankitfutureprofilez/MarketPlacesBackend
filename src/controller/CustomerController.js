4b89edcd62a1ee772ec8b2fca51a91c10a2285f7
merndevfp01-ctrl <mern.dev.fp01@gmail.com>
2026-04-13 16:31:20 +0530

    Update Customer API

--- src/controller/CustomerController.js
+++ src/controller/CustomerController.js
@@ -162,13 +162,23 @@ const getVendorsWithMaxOffer = async (vendors) => {
 
         // console.log(vendor?.user);
         console.log("vendor?.user?._id",vendor?.user);
-        const vendorId = vendor?.user?._id ? new mongoose.Types.ObjectId(vendor?.user?._id) : '';
-        // console.log("vendor",vendorId);
-        // Fetch all active offers for the vendor
-        const offers = await Offer.find({ vendor: vendorId, status: "active" })
-          .populate("flat")
-          .populate("percentage");
-  
+
+        const userId = vendor?.user?._id || vendor?.user;
+
+        if (!userId) {
+          return res.json({
+            success: true,
+            activeOffersCount: 0,
+          });
+        }
+
+        const vendorId = new mongoose.Types.ObjectId(userId);
+
+        const offers = await Offer.find({
+          vendor: vendorId,
+          status: "active",
+        });
+
         const activeOffersCount = offers.length;
   
         if (activeOffersCount === 0) {
